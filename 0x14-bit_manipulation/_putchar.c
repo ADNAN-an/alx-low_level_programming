bbac9189eb6e44fb5038de22a2487f53dcac4926
ADNAN-an <adnan.anjar2002@gmail.com>
2023-05-05 18:54:16 -0700

    test

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * On success 1 is returned.
- * On error, -1 is returned.
+ *Return: On success 1 is returned.
+ *On error, -1 is returned and errno is set appropriately
  */
 int _putchar(char c)
 {
